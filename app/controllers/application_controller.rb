5054298badd16e9d6e80ee30eb0979f653c5e0d7
xoff69 <xoff.pichon@gmail.com>
2023-01-06 15:05:27 -0500

    version de depart fonctionnelle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+
+  before_action :authenticate_user!
+  before_action :turbo_frame_request_variant
+
+
+
+  protected
+  def after_sign_up_path_for(resource)
+     # @FIXME boards_index_path
+  end
+
+  def turbo_frame_request_variant
+      request.variant = :turbo_frame if turbo_frame_request?
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   resources :boards
   resources :tickets
   root "boards#index"
